b52a3c98472d1a86619ce6210bb80aee958a84fe
Александр <avsimvolokov@gmail.com>
2025-08-10 16:12:07 +0300

    Finished 1 and 2 part from day5

--- Day-5/Day-5.playground/Contents.swift
+++ Day-5/Day-5.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-let score = 85
+var score = 85
 
 if score > 80 {
     print("Great job!")
@@ -8,7 +8,7 @@ if score > 80 {
 
 let speed = 88
 let percentage = 85
-let age = 18
+var age = 18
 
 if speed >= 88 {
     print("Where we're going we don't need roads.")
@@ -82,4 +82,138 @@ if username.isEmpty == true {
 
 if username.isEmpty {
     username = "Anonymous"
+}
+
+let firstName = "Paul"
+let secondName = "Sophie"
+
+let firstAge = 40
+let secondAge = 10
+
+print(firstName == secondName)
+print(firstName != secondName)
+print(firstName < secondName)
+print(firstName >= secondName)
+
+print(firstAge == secondAge)
+print(firstAge != secondAge)
+print(firstAge < secondAge)
+print(firstAge >= secondAge)
+
+enum Sizes: Comparable {
+    case small
+    case medium
+    case large
+}
+
+let first = Sizes.small
+let second = Sizes.large
+print(first < second)
+
+age = 16
+
+if age >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let a = false
+let b = true
+
+if a {
+    print("Code to run if a is true")
+} else if b {
+    print("Code to run if a is false but b is true")
+} else {
+    print("Code to run if both a and b are false")
+}
+
+let temp = 25
+
+if temp > 20 {
+    if temp < 30 {
+        print("It's a nice day.")
+    }
+}
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if userAge >= 18 || hasParentalConsent == true {
+    print("You can buy the game")
+}
+
+if userAge >= 18 || hasParentalConsent {
+    print("You can buy the game")
+}
+
+enum TransportOption {
+    case airplane, helicopter, bicycle, car, scooter
+}
+
+let transport = TransportOption.airplane
+
+if transport == .airplane || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bike path…")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
+
+score = 9001
+
+if score > 9000 {
+    print("It's over 9000!")
+} else {
+    print("It's not over 9000!")
+}
+
+if score > 9000 {
+    print("It's over 9000!")
+} else {
+    if score == 9000 {
+        print("It's exactly 9000!")
+    } else {
+        print("It's not over 9000!")
+    }
+}
+
+if score > 9000 {
+    print("It's over 9000!")
+} else if score == 9000 {
+    print("It's exactly 9000!")
+} else {
+    print("It's not over 9000!")
+}
+
+var isOwner = true
+var isAdmin = true
+var isEditingEnabled = false
+
+if isOwner == true || isAdmin == true {
+    print("You can delete this post")
+}
+
+if isOwner == true && isEditingEnabled || isAdmin == true {
+    print("You can delete this post")
+}
+
+if (isOwner == true && isEditingEnabled) || isAdmin == true {
+    print("You can delete this post")
+}
+
+if isOwner == true && (isEditingEnabled || isAdmin == true) {
+    print("You can delete this post")
+}
+
+if (isOwner == true && isEditingEnabled) || isAdmin == true {
+    print("You can delete this post")
 }
