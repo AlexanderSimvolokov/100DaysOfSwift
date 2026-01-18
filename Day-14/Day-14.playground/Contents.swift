2de54301bd3f2166f21d9b5eef546dce0fc4ac2f
Александр <avsimvolokov@gmail.com>
2026-01-18 23:52:59 +0300

    Completed day 14

---
+++ Day-14/Day-14.playground/Contents.swift
@@ -0,0 +1,100 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+let peachOpposite = opposites["Peach"]
+
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
+
+func square(number: Int) -> Int {
+    number * number
+}
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+
+        // 1: We *must* exit the function here
+        return
+    }
+
+    // 2: `number` is still available outside of `guard`
+    print("\(number) x \(number) is \(number * number)")
+}
+
+func getMeaningOfLife() -> Int? {
+    42
+}
+
+func printMeaningOfLife() {
+    guard let name = getMeaningOfLife() else {
+        return
+    }
+
+    print(name)
+}
+
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+let favorite = tvShows.randomElement() ?? "None"
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let number = Int(input) ?? 0
+print(number)
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+
+struct Book1 {
+    let title: String
+    let author: String?
+}
+
+var book1: Book1? = nil
+let author1 = book1?.author?.first?.uppercased() ?? "A"
+print(author1)
+
+let names1 = ["Vincent": "van Gogh", "Pablo": "Picasso", "Claude": "Monet"]
+let surnameLetter = names1["Vincent"]?.first?.uppercased()
+
+let surnameLetter1 = names1["Vincent"]?.first?.uppercased() ?? "?"
+
+enum UserError: Error {
+    case badID, networkFailed
+}
+
+func getUser(id: Int) throws -> String {
+    throw UserError.networkFailed
+}
+
+if let user = try? getUser(id: 23) {
+    print("User: \(user)")
+}
+
+let user = (try? getUser(id: 23)) ?? "Anonymous"
+print(user)
