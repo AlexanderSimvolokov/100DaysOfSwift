f290533762fe32a193053093222f241dc84c3c00
Александр <avsimvolokov@gmail.com>
2026-02-01 18:21:21 +0300

    Completed day 16

---
+++ Day-16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Alexander Simvolokov on 31.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
