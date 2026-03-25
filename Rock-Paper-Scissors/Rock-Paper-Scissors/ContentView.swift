929027abd4ecf73c30d53bcb86d3ac88ddc1fd88
Александр <avsimvolokov@gmail.com>
2026-03-25 15:31:41 +0300

    Added comparison conditions

--- Rock-Paper-Scissors/Rock-Paper-Scissors/ContentView.swift
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     
     @State var youChose = "?"
     @State var opponentChose = "?"
+    @State var youMadeaChoise = false
+    @State var opponentMadeaChoise = false
     
     var body: some View {
         ZStack {
@@ -24,16 +26,29 @@ struct ContentView: View {
                     .background(.green)
             }
         }
+        .onChange(of: youMadeaChoise && opponentMadeaChoise) { isBothReady in
+            if isBothReady {
+                startGame()
+            }
+        }
     }
     
     @ViewBuilder
     private func playerSection(isTop: Bool) -> some View {
         VStack(spacing: 0) {
             VStack {
+                Text("Безопасная зона")
+                    .foregroundStyle(Color.black)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 50)
+            .background(Color.yellow.opacity(0.4))
+            
+            VStack {
                 Text(isTop ? "Противник выбрал" : "Вы выбрали")
                     .font(.title2)
-                Text(isTop ? opponentChose : youChose)
-                    .font(.caption)
+                
+                Text(isTop ? opponentMadeaChoise ? "..." : opponentChose : youMadeaChoise ? "..." :youChose)
+                    .font(.largeTitle)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(isTop ? Color.blue.opacity(0.2) : Color.green.opacity(0.2))
@@ -59,7 +74,7 @@ struct ContentView: View {
             Spacer()
             
             Button {
-                
+                choose(isTop: isTop)
             } label: {
                 Text(isTop ? "Выбрать" : "Выбрать")
                     .font(.largeTitle.bold())
@@ -75,6 +90,49 @@ struct ContentView: View {
         }
         .foregroundStyle(.white)
     }
+    
+    func choose(isTop: Bool) {
+        if isTop {
+            opponentMadeaChoise.toggle()
+        } else {
+            youMadeaChoise.toggle()
+        }
+    }
+    
+    func startGame() {
+        if youChose == "Rock" && opponentChose == "Scissors" {
+            youChose = "Вы выйграли!!!"
+            opponentChose = "Вы проиграли((("
+        } else if  youChose == "Rock" && opponentChose == "Paper" {
+            youChose = "Вы проиграли((("
+            opponentChose = "Вы выйграли!!!"
+        } else if  youChose == "Rock" && opponentChose == "Rock" {
+            youChose = "Ничья"
+            opponentChose = "Ничья"
+        } else if  youChose == "Paper" && opponentChose == "Rock" {
+            youChose = "Вы выйграли!!!"
+            opponentChose = "Вы проиграли((("
+        } else if  youChose == "Paper" && opponentChose == "Scissors" {
+            youChose = "Вы проиграли((("
+            opponentChose = "Вы выйграли!!!"
+        } else if  youChose == "Paper" && opponentChose == "Paper" {
+            youChose = "Ничья"
+            opponentChose = "Ничья"
+        } else if  youChose == "Scissors" && opponentChose == "Paper" {
+            youChose = "Вы выйграли!!!"
+            opponentChose = "Вы проиграли((("
+        } else if  youChose == "Scissors" && opponentChose == "Rock" {
+            youChose = "Вы проиграли((("
+            opponentChose = "Вы выйграли!!!"
+        } else if  youChose == "Scissors" && opponentChose == "Scissors" {
+            youChose = "Ничья"
+            opponentChose = "Ничья"
+        }
+        
+        youMadeaChoise.toggle()
+        opponentMadeaChoise.toggle()
+    }
+    
 }
 
 #Preview {
