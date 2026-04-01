11e5ec7cae268e9c277dfd72d404c3b8a2bc3114
Александр <avsimvolokov@gmail.com>
2026-04-01 12:05:45 +0300

    Finished day 26

---
+++ Day-26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alexander Simvolokov on 27.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
