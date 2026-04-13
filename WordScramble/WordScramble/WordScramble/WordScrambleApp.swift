19988d4de4499c4d8198c3f66b0ef4a3dffe1831
Александр <avsimvolokov@gmail.com>
2026-04-13 22:55:41 +0300

    Finished day29

---
+++ WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Alexander Simvolokov on 10.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
