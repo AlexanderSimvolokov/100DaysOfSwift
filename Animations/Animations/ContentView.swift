d1021b35b2e6fd4d49f3916e087fe37be24a7a7a
Александр <avsimvolokov@gmail.com>
2026-05-06 08:18:03 +0300

    Finished day 33

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,97 +7,90 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
     
-    //    @State private var animationAmount = 1.0
-    @State private var animationAmount = 0.0
-    
-    var body: some View {
-        print(animationAmount)
-        
-        return VStack {
-            Button("Tap Me") {
-                animationAmount += 1
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .onAppear {
-                animationAmount = 2
-            }
-            //            .scaleEffect(animationAmount).blur(radius: (animationAmount - 1) * 3)
-            //.animation(.default, value: animationAmount)
-            //.animation(.linear, value: animationAmount)
-            //            .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
-            //            .animation(.easeInOut(duration: 2), value: animationAmount)
-            //            .animation(
-            //                .easeInOut(duration: 2)
-            //                    .delay(1),
-            //                value: animationAmount
-            //            )
-            //            .animation(
-            //                .easeInOut(duration: 1)
-            //                    .repeatCount(3, autoreverses: true),
-            //                value: animationAmount
-            //            )
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmount)
-                    .opacity(2 - animationAmount)
-                    .animation(
-                        .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                        value: animationAmount
-                    )
-            )
-            .animation(
-                .easeInOut(duration: 1)
-                .repeatForever(autoreverses: true),
-                value: animationAmount
-            )
-//            VStack {
-//                //                        Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
-//                Stepper("Scale amount", value: $animationAmount.animation(
-//                    .easeInOut(duration: 1)
-//                    .repeatCount(3, autoreverses: true)
-//                ), in: 1...10)
-//                
-//                Spacer()
-//                
-//                Button("Tap Me") {
-//                    animationAmount += 1
-//                }
-//                .padding(40)
-//                .background(.red)
-//                .foregroundStyle(.white)
-//                .clipShape(.circle)
-//                .scaleEffect(animationAmount)
-//            }
-            .padding(100)
-            Spacer()
-            
-            Button("Tap Me") {
-//                withAnimation {
-//                    animationAmount += 360
-//                }
-                withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                    animationAmount += 360
-                }
-            }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
-        }
-        .padding()
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
     }
-    
 }
 
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
 
+struct ContentView: View {
+    
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    @State private var isShowingRed = false
+    
+    
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
+            }
+        }
+        
+        
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+    }
+}
 
 #Preview {
     ContentView()
