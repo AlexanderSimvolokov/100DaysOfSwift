991a3e637a40ca05d498f64ea63b171384af58cd
Александр <avsimvolokov@gmail.com>
2026-05-14 07:21:17 +0300

    Start day35

---
+++ MultiplicationTables/MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Alexander Simvolokov on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
