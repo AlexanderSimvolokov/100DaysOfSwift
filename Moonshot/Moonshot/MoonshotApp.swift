1deea894a1a178b595710880b8213bac76829ce4
Александр <avsimvolokov@gmail.com>
2026-05-31 18:39:43 +0300

    Finished day 39

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Alexander Simvolokov on 30.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
