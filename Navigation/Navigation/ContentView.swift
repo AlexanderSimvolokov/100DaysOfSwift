3a0de74f8efd6918888e3194c439be9e8dbfcda7
Александр <avsimvolokov@gmail.com>
2026-06-04 23:28:29 +0300

    Finished day43

---
+++ Navigation/Navigation/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Alexander Simvolokov on 04.06.2026.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var number: Int
+
+    var body: some View {
+        Text("Detail View \(number)")
+    }
+
+    init(number: Int) {
+        self.number = number
+        print("Creating detail view \(number)")
+    }
+}
+
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                    Text("You selected \(selection)")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
