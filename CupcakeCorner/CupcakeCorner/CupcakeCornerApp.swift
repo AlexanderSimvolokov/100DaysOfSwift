ecd205a43353d02085f49424f766afd14d6fff44
Александр <avsimvolokov@gmail.com>
2026-07-05 12:23:41 +0300

    Finished day 49

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alexander Simvolokov on 22.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
